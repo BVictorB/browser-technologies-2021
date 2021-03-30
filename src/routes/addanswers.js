c19861fe80b4f0a79962a7f9933054574c303dcc
bvictorb <victory-boucher@hotmail.com>
2021-03-30 13:10:11 +0200

    fixed add answers without javascript

--- src/routes/addanswers.js
+++ src/routes/addanswers.js
@@ -1,6 +1,10 @@
 const addanswers = (req, res) => {
-  const answers = Array.from(Array(Number(req.body.answers)).keys())
-  res.render('pages/createpoll', { answers })
+  const 
+    today = new Date().toISOString().slice(0, 10),
+    week = new Date(new Date().getTime() + (60*60*24*7*1000)).toISOString().slice(0, 10),
+    answers = Array.from(Array(Number(req.body.answers)).keys())
+
+  res.render('pages/createpoll', { answers, today, week })
 }
 
 module.exports = addanswers
